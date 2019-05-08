ce3118e4d37fea85533462b0b835365979dae291
Riba0017 <Riba0017@yandex.ru>
2019-05-08 19:32:38 +0300

    dom

--- js_practice/hw-dom.js
+++ js_practice/hw-dom.js
@@ -1,3 +1,110 @@
 
 //1
+// Дан элемент #elem. Добавьте ему класс "www".
 
+let elem = document.getElementById('elem');
+let elemWithClass = elem.classList.add("www");
+console.log(elem);
+
+// 2
+// Дан элемент #elem. Проверьте наличие у него класса "www",
+// если есть - удалите класс
+
+if(elem.classList.contains('www')) {
+    elem.classList.remove('www');
+}
+console.log(elem);
+
+// 3
+// Напишите функцию поиска recursiveSearchTags, которая принимает
+// на вход document и имя тега, а возвращает массив из всех элементов
+// соответствующих этому тегу. Поиск идет в теге body
+
+function recursiveSearchTags(document, tag) {
+    let arr = [];
+    function innerFunction(arr, target, tag) {
+        for (let i = 0; i < target.children.length; i++) {
+            let el = target.children[i];
+            if (arr.indexOf(el) !== -1)
+                break;
+            if (el.tagName === tag.toUpperCase())
+                arr.push(el);
+            if (el.hasChildNodes())
+                innerFunction(arr, el, tag);
+        }
+    }
+   for (let i = 0; i < document.body.children.length; i++)
+        innerFunction(arr, document.body, tag);
+   return arr;
+}
+console.log(recursiveSearchTags(document, 'input'));
+
+// 4
+// Дан ul. Дан массив. Вставьте элементы этого массива в конец ul так,
+// чтобы каждый элемент стоял в своем li. Сделайте так, чтобы четные
+// позиции имели красный фон.
+
+let elementsArr = ['a', 'span', 'p'];
+let needUl = document.getElementsByClassName('dom-list')[0];
+
+function addElements(list, arr) {
+    for(let i = 0; i < arr.length; i++) {
+        let wrapperLi = document.createElement('li');
+        list.appendChild(wrapperLi);
+        let innerElem = document.createElement(arr[i]);
+        innerElem.innerHTML = "i am new " + arr[i];
+        wrapperLi.appendChild(innerElem);
+    }
+    let even = list.querySelectorAll('li:nth-child(2n)');
+    for(let j = 0; j < even.length; j++) {
+        even[j].style.backgroundColor = "red";
+    }
+}
+console.log(addElements(needUl, elementsArr));
+
+// 5
+// Дан элемент #elem. Найдите его соседа сверху и добавьте ему в конец текст '!'
+
+let previos = elem.previousElementSibling;
+let text = document.createTextNode('!');
+previos.appendChild(text);
+
+// 6
+// Реализуйте функцию wrapInParagraph, которая находит текст
+// (дочерние текстовые ноды) внутри элемента div и оборачивает текст в параграф
+
+let divElems = document.getElementsByTagName('div');
+for(let i = 0; i < divElems.length; i++) {
+    let node = divElems[i].childNodes;
+    for(let j = 0; j < node.length; j++) {
+        if((node[j].nodeType === 3) && (node[j].data.match(/\S/g))) {
+            let parag = document.createElement('p');
+            let newAdd = divElems[i].appendChild(parag);
+            newAdd.appendChild(node[j]);
+        }
+    }
+}
+
+// 7
+// Реализуйте функцию normalizeClassNames, которая нормализует имена классов
+// для всех элементов на странице. В данном случае это означает что происходит
+// преобразование всех классов написанных используя kebab нотацию в camelCase
+// нотацию: text-center => textCenter
+
+let children = document.body.getElementsByTagName('*');
+let classes = [].filter.call(children, (item) => {
+    return item.hasAttribute('class');
+});
+    classes.forEach(item => {
+    let kebabClass = item.getAttribute('class');
+    if(kebabClass.indexOf('-') !== -1) {
+        let arr = kebabClass.split('-');
+        for(let i = 1; i < arr.length; i++) {
+            arr[i] = arr[i].split('');
+            arr[i][0] = arr[i][0].toUpperCase();
+            arr[i] = arr[i].join('');
+        }
+        let camel = arr.join('');
+        let camelClass = item.setAttribute('class', camel);
+    }
+});
--- js_practice/practice-dom.js
+++ js_practice/practice-dom.js
@@ -4,46 +4,56 @@
 
 let table = document.getElementsByTagName('table')[0];
 let labels = table.getElementsByTagName('label');
-// console.log(labels);
+console.log(labels);
 
 //2
 // Первую ячейку таблицы (со словом "Возраст").
 
 // let firstTr = table.getElementsByTagName('tr')[0].firstElementChild;
 let firstTr = table.getElementsByTagName('td')[0];
-// console.log(firstTr);
+console.log(firstTr);
 
 //3
 // Вторую форму в документе.
 
 let secondForm = document.getElementsByTagName('form')[1];
-// console.log(secondForm);
+console.log(secondForm);
 
 //4
 // Форму с именем search, без использования её позиции в документе.
 
-let searchForm1 = document.getElementsByName('search');
-for(let i = 0; i < searchForm1.length; i++) {
-    if(searchForm1[i].nodeName === 'FORM') {
-        console.log(searchForm1[i]);
-        break;
-    }
-}
+// let searchForm1 = document.getElementsByName('search');
+// for(let i = 0; i < searchForm1.length; i++) {
+//     if(searchForm1[i].nodeName === 'FORM') {
+//         console.log(searchForm1[i]);
+//         break;
+//     }
+// }
 //еще вариант
 let searchForm = document.querySelector('form[name="search"]');
-// console.log(searchForm);
+console.log(searchForm);
 
 //5
 // Элемент input в форме с именем search. Если их несколько,
 // то нужен первый.
 
+let firstInput = searchForm.querySelector('input');
+console.log(firstInput);
+
 //6
 // Элемент с именем info[0], без точного знания его позиции в документе.
 
+let info = document.getElementsByName('info[0]')[0];
+console.log(info);
 
 //7
 // Элемент с именем info[0], внутри формы с именем search-person.
 
+let form = document.querySelector('form[name="search-person"]').elements;
+let newArr = [].filter.call(form, (item) => {
+    return item.name === 'info[0]';
+});
+console.log(newArr);
 
 //8
 // Нужно создать пустой html документ и заполнить его используя
@@ -60,5 +70,12 @@ let searchForm = document.querySelector('form[name="search"]');
 // Сделайте красными все внешние ссылки (имеющие начало с http://),
 // поменяйте местами два произвольных элемента списка
 
+let list = document.getElementsByClassName('dom-list')[0];
+let first = list.children[2];
+let second = list.children[4];
+list.insertBefore(second, first);
 
-
+let elems = document.querySelectorAll('a[href^="http://"]');
+for(let i = 0; i < elems.length; i++) {
+    elems[i].style.color = 'red';
+}
