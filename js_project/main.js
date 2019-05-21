d4d6c0e97ecd2379393ba1847be06a720edc69d0
Riba0017 <Riba0017@yandex.ru>
2019-05-21 22:27:07 +0300

    add a pic in pop-up

--- js_project/main.js
+++ js_project/main.js
@@ -1,20 +1,20 @@
-
 window.addEventListener('DOMContentLoaded', () => {
     let main = document.getElementsByTagName('main')[0];
 
     (function getBD(basis) {
         basis.forEach(item => {
             let container = document.createElement('div');
-            container.className = "product-item";
+            container.className = 'product-item';
+            container.setAttribute('id', item.id);
             main.appendChild(container);
 
             let card = document.createElement('div');
-            card.className = "item-photo";
+            card.className = 'item-photo';
             card.style.backgroundImage = item.photo;
             container.appendChild(card);
 
             let cardName = document.createElement('h3');
-            cardName.innerHTML = "Исполнитель: " + item.singer;
+            cardName.innerHTML = 'Исполнитель: ' + item.singer;
             container.appendChild(cardName);
         })
     })(bd);
@@ -25,7 +25,20 @@ window.addEventListener('DOMContentLoaded', () => {
 
     document.addEventListener('click', (event) => {
         if(event.target.className === "item-photo") {
-            pop.classList.toggle('close');
+            pop.classList.remove('close');
+            let activeId = event.target.parentNode.getAttribute('id');
+
+            (function detailItem(basis) {
+                basis.forEach(item => {
+                    if(item.id == activeId) {
+                        // console.log('entry');
+                        let detailPhoto = document.createElement('div');
+                        detailPhoto.className = 'item-photo';
+                        detailPhoto.style.backgroundImage = item.photo;
+                        pop.appendChild(detailPhoto);
+                    }
+                })
+            })(bd);
         }
     });
 
