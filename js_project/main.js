a5bcf47029750654057738672827e1cef26af315
Riba0017 <Riba0017@yandex.ru>
2019-05-27 21:30:18 +0300

    update

--- js_project/main.js
+++ js_project/main.js
@@ -1,4 +1,8 @@
 window.addEventListener('DOMContentLoaded', () => {
+    /*записываем исходные данные в localStorage*/
+    localStorage.setItem('bd', JSON.stringify(bd));
+    let bdLocal = JSON.parse(localStorage.getItem('bd'));
+
     let main = document.getElementsByTagName('main')[0];
 
     /*генерация карточек товара из базы*/
@@ -18,7 +22,7 @@ window.addEventListener('DOMContentLoaded', () => {
             cardName.innerHTML = 'Исполнитель: ' + item.singer;
             container.appendChild(cardName);
         })
-    })(bd);
+    })(bdLocal);
 
     /*создание и добавление элементов затемнения фона, поп-апа, кнопки закрытия поп-апа*/
     let popBack = document.createElement('div');
@@ -30,6 +34,9 @@ window.addEventListener('DOMContentLoaded', () => {
     let closeBtn = document.createElement('div');
     closeBtn.className = 'close-button';
     pop.appendChild(closeBtn);
+    let detailPhoto = document.createElement('div');
+    let detailDescription = document.createElement('div');
+    let detailParagraph = document.createElement('p');
 
     /*событие клика по карточке товара - открытие поп-апа с детальной информацией*/
     document.addEventListener('click', (event) => {
@@ -38,12 +45,8 @@ window.addEventListener('DOMContentLoaded', () => {
             pop.classList.remove('close');
 
             /*создание внутренних элементов поп-апа*/
-            let detailPhoto = document.createElement('div');
             detailPhoto.className = 'item-photo';
-            let detailDescription = document.createElement('div');
             detailDescription.className = 'item-description';
-            let detailParagraph = document.createElement('p');
-
 
             /*проверка на повторное открытие с одной и той же карточки товара*/
             if(pop.dataset.id == event.target.parentNode.dataset.id) {
@@ -52,23 +55,23 @@ window.addEventListener('DOMContentLoaded', () => {
                 /*изменение детального описания при открытии другого поп-апа*/
                 pop.dataset.id = event.target.parentNode.dataset.id;
                 if(pop.querySelector('.item-photo')) {
-                    for(let i = 0; i < bd.length; i++) {
-                        if(bd[i].id == pop.dataset.id) {
-                            pop.querySelector('.item-photo').style.backgroundImage = bd[i].photo;
-                            pop.querySelector('[data-singer]').innerHTML = 'Исполнитель: ' + bd[i].singer;
+                    for(let i = 0; i < bdLocal.length; i++) {
+                        if(bdLocal[i].id == pop.dataset.id) {
+                            pop.querySelector('.item-photo').style.backgroundImage = bdLocal[i].photo;
+                            pop.querySelector('[data-singer]').innerHTML = 'Исполнитель: ' + bdLocal[i].singer;
                             break;
                         }
                     }
                 } else {
                     /*первое создание детального описания в поп-апе*/
-                    for(let i = 0; i < bd.length; i++) {
-                        if(bd[i].id == pop.dataset.id) {
-                            detailPhoto.style.backgroundImage = bd[i].photo;
+                    for(let i = 0; i < bdLocal.length; i++) {
+                        if(bdLocal[i].id == pop.dataset.id) {
+                            detailPhoto.style.backgroundImage = bdLocal[i].photo;
                             pop.appendChild(detailPhoto);
                             pop.appendChild(detailDescription);
                             detailParagraph.dataset.singer = 'singer';
                             pop.querySelector('.item-description').appendChild(detailParagraph);
-                            detailDescription.querySelector('[data-singer]').innerHTML = 'Исполнитель: ' + bd[i].singer;
+                            detailDescription.querySelector('[data-singer]').innerHTML = 'Исполнитель: ' + bdLocal[i].singer;
                             break;
                         }
                     }
@@ -80,6 +83,28 @@ window.addEventListener('DOMContentLoaded', () => {
     closeBtn.addEventListener('click', () => {
         popBack.classList.add('close');
         pop.classList.add('close');
-    })
+    });
+
+    let detailForm = document.createElement('form');
+    let input = document.createElement('input');
+    input.setAttribute('name', 'message');
+    input.setAttribute('type', 'text');
+    let inputBtn = document.createElement('input');
+    inputBtn.className = 'write-button';
+    inputBtn.innerHTML = 'write-button';
+    inputBtn.setAttribute('type', 'submit');
+    detailDescription.appendChild(detailForm);
+    detailForm.appendChild(input);
+    detailForm.appendChild(inputBtn);
+
+    console.log(detailDescription.getElementsByTagName('form')[0]);
+
+    detailDescription.getElementsByTagName('form')[0].onsubmit = function() {
+        let message = detailForm.querySelector('input[name="message"]').value;
+
+        // localStorage.setItem('', message);
+        // console.log();
+        return false;
+    };
 
 });
